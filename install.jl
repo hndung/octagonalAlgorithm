285180cf60bb0081781d762e5f5c81ed6c7e9398
hndung <30950958+hndung@users.noreply.github.com>
2024-03-04 17:12:30 +0700

    Update install.jl

--- install.jl
+++ install.jl
@@ -1,2 +1,2 @@
 using Pkg; Pkg.add(["JLD2","Printf","CSV", "DataFrames","StatsBase",
-"Distributions","TimerOutputs","QHull","BenchmarkTools"])
+"Distributions","TimerOutputs","QHull","BenchmarkTools", "IterTools", "CGAL"])
